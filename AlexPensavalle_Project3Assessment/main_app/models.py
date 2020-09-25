50196cb4b93e621870f3b7fa286aaf89f2c74599
Alexander Pensavalle <alexanderpensavalle@Alexanders-MacBook-Pro.local>
2020-09-25 14:51:13 -0400

    Complete Step 3.1

--- AlexPensavalle_Project3Assessment/main_app/models.py
+++ AlexPensavalle_Project3Assessment/main_app/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+from django.urls import reverse
+
+class Widget(models.Model):
+    item = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.item
+
+    def get_absolute_url(self):
+        return reverse('index')
--- AlexPensavalle_Project3Assessment/main_app/views.py
+++ AlexPensavalle_Project3Assessment/main_app/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Widget
+
+def index(request):
+    widgets = Widget.objects.all()
+    return render(request, 'index.html', {'widgets': widgets})
